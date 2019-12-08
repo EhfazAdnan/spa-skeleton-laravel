78ec5989f8474cc537490c355c086ac3d7bed496
EhfazAdnan <ehfaz15-167@diu.edu.bd>
2019-12-08 21:43:03 +0600

    Laravel SPA Skeleton VueJs

---
+++ resources/js/components/About.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>About Page</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <h1>Home Page</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
